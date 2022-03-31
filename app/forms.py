1890a23180e7b4181e73c48c3955929b0160a9c7
Piotr Tyran <piotrtyran@tutanota.com>
2022-03-31 18:32:11 +0200

    Dodanie klasy RegistrationForm i wymaganych importów do pliku app/forms.py.

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,28 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo
+from app.models import Users
 
 
 class LoginForm(FlaskForm):
     username = StringField('Użytkownik: ', validators=[DataRequired()])
     password = PasswordField('Hasło: ', validators=[DataRequired()])
     submit = SubmitField('Zaloguj się')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Użytkownik: ', validators=[DataRequired()])
+    email = StringField('Adres email: ', validators=[DataRequired(), Email()])
+    password = PasswordField('Hasło: ', validators=[DataRequired()])
+    password_repeat = PasswordField(
+        'Powtórz hasło: ', validators=[DataRequired(),
+                                       EqualTo('password', message='Hasła muszą się zgadzać.')])
+    submit = SubmitField('Zarejestruj się')
+
+    def validate_username(self, username):
+        if Users.query.filter_by(username=username.data).first() is not None:
+            raise ValidationError('Nazwa użytkownika jest zajęta.')
+
+    def validate_email(self, email):
+        if Users.query.filter_by(email=email.data).first() is not None:
+            raise ValidationError('Adres email jest już zajęty.')
